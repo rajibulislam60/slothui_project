7d1db5e882535fa0f07cf80babad87e4f33d9a7f
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-04-24 20:05:55 +0600

    counting area done

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,37 @@
+function toggleMenu() {
+  document.querySelector(".list").classList.toggle("show");
+  document.querySelector(".auth-buttons").classList.toggle("show");
+}
+// -------------- Counting area------------
+const counters = document.querySelectorAll(".countingItem");
 
-  function toggleMenu() {
-    document.querySelector('.list').classList.toggle('show');
-    document.querySelector('.auth-buttons').classList.toggle('show');
-  }
+counters.forEach((counter) => {
+  const target = parseFloat(counter.getAttribute("data-target"));
+  let current = 0;
+  const speed = 0.02;
+  const delay = 10;
+
+  const suffix = counter.innerText.replace(/[0-9.,]/g, "");
+
+  const updateCount = () => {
+    current += target * speed;
+
+    if (current < target) {
+      if (suffix === "M") {
+        counter.innerText = current.toFixed(2) + suffix;
+      } else {
+        counter.innerText = Math.floor(current).toLocaleString() + suffix;
+      }
+      setTimeout(updateCount, delay);
+    } else {
+      if (suffix === "M") {
+        counter.innerText = target.toFixed(2) + suffix;
+      } else {
+        counter.innerText = target.toLocaleString() + suffix;
+      }
+    }
+  };
+
+  updateCount();
+});
+
